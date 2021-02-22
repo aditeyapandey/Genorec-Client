b18638021bd4f68c27c85e733d8898bcd5855e69
Sehi L'Yi <sehilyi@gmail.com>
2021-02-23 05:09:59 +0900

    Add a basic gosling output on the recommendation panel (#2)

--- src/Recommendation/recommendation.js
+++ src/Recommendation/recommendation.js
@@ -1,8 +1,28 @@
 import React from "react";
+import { GoslingComponent } from "gosling.js";
+
 
 class Recommendation extends React.Component {
     render() {
-      return <h1>Hello, {this.props.data}</h1>;
+      return (
+        <GoslingComponent
+          spec={{
+            "tracks": [
+              {
+                "data": {
+                  "url": "https://resgen.io/api/v1/tileset_info/?d=UvVPeLHuRDiYA3qwFlm7xQ",
+                  "type": "multivec",
+                  "row": "sample",
+                  "column": "position",
+                  "value": "peak",
+                  "categories": ["sample 1", "sample 2", "sample 3", "sample 4"]
+                }
+              }
+            ]
+          }}
+          compiled={(spec, vConf) => { /* Callback function when compiled */ }}
+        />
+      );
     }
   }
 
