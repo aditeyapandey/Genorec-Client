113754b350c4b4f5c1b19abfc3a92104a39e56d0
Aditeya Pandey <aditeyapandey@gmail.com>
2021-03-06 19:41:02 -0500

    Resolcing conflicts

--- src/Inputpage/Inputpage.js
+++ src/Inputpage/Inputpage.js
@@ -516,7 +516,7 @@ class Inputpage extends React.Component {
                       </h3>
                     </div>
                   </div>
-                  <div className="w3-row w3-center w3-display-container w3-margin">
+                  <div className="w3-row w3-center w3-display-container w3-padding w3-margin">
                     <Recommendation className="w3-center" data={this.state.recommendationOutputSpec} width={800}/>
                   </div>
                 </div>
