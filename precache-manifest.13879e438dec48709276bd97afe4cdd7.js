948ea66f491afa0bc6f1486162185c76ac54ee9e
Sehi L'Yi <sehi_lyi@hms.harvard.edu>
2021-03-10 15:10:40 -0500

    Updates

---
+++ precache-manifest.13879e438dec48709276bd97afe4cdd7.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "527a62c0effa2b591f2fdfc1b220e50b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b80b087625a8e2aa3549",
+    "url": "/static/css/main.b3a44951.chunk.css"
+  },
+  {
+    "revision": "3c61c1fd83343656e999",
+    "url": "/static/js/2.cb78488f.chunk.js"
+  },
+  {
+    "revision": "df73b91622264f000ef2eff364f98bbe",
+    "url": "/static/js/2.cb78488f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b80b087625a8e2aa3549",
+    "url": "/static/js/main.ef360e5c.chunk.js"
+  },
+  {
+    "revision": "deb782f1397c7e94a833",
+    "url": "/static/js/runtime-main.e08225b5.js"
+  },
+  {
+    "revision": "8eb3cd2d1d8783fcffb14fcd4d6fae23",
+    "url": "/static/media/annotation.8eb3cd2d.jpg"
+  },
+  {
+    "revision": "efb732d71d92b59587b028d544f1ad67",
+    "url": "/static/media/areachart.efb732d7.pdf"
+  },
+  {
+    "revision": "7f895b0ac062f3fd6b2fe8b1d7f27bd1",
+    "url": "/static/media/areahue.7f895b0a.jpg"
+  },
+  {
+    "revision": "a72801764fc4e06c1b9cf7b28c787371",
+    "url": "/static/media/areasaturation.a7280176.jpg"
+  },
+  {
+    "revision": "57c8c514feb1f4dba5a10d01521a91e4",
+    "url": "/static/media/areasize.57c8c514.jpg"
+  },
+  {
+    "revision": "0b7707eb3f544d5d466a259227d23f2f",
+    "url": "/static/media/barhue.0b7707eb.jpg"
+  },
+  {
+    "revision": "abf4cf535519fd08ab36d7d57048bc47",
+    "url": "/static/media/barsaturation.abf4cf53.jpg"
+  },
+  {
+    "revision": "d3ff128fb45951d40d7b3d504b9ea544",
+    "url": "/static/media/barsize.d3ff128f.jpg"
+  },
+  {
+    "revision": "8ca9c42b5e28464108ff7c49d54cf54c",
+    "url": "/static/media/circular.8ca9c42b.jpg"
+  },
+  {
+    "revision": "d7489665fa7df038008c8a6177cb4073",
+    "url": "/static/media/dotplot.d7489665.jpg"
+  },
+  {
+    "revision": "4a14a220888a50fd154aaca78e393399",
+    "url": "/static/media/linear.4a14a220.jpg"
+  },
+  {
+    "revision": "2e428433f4fe1155b17c566542f9da7c",
+    "url": "/static/media/linechart.2e428433.jpg"
+  },
+  {
+    "revision": "0b2e3adae3477228e2bdafcd952d1ef4",
+    "url": "/static/media/multifeature.0b2e3ada.jpeg"
+  },
+  {
+    "revision": "c6c26484451db16f0beaa8833220ee55",
+    "url": "/static/media/multifeatures.c6c26484.png"
+  },
+  {
+    "revision": "9034691278821c49c34683106c60a80a",
+    "url": "/static/media/multipleattributes.90346912.png"
+  },
+  {
+    "revision": "d4429721a7b9dbcca0c40daeed3578ec",
+    "url": "/static/media/multiplefeatures.d4429721.png"
+  },
+  {
+    "revision": "679721e5ca8c978980c7f107d6c4483a",
+    "url": "/static/media/multipleroi.679721e5.png"
+  },
+  {
+    "revision": "8599ce2c75d2836639132424094b70d6",
+    "url": "/static/media/multipleroi.8599ce2c.jpeg"
+  },
+  {
+    "revision": "09e7a210d8c66a7d3de594a380335ece",
+    "url": "/static/media/multiplesequences.09e7a210.png"
+  },
+  {
+    "revision": "5630ba2360786ac9a6c977f98e38d2ed",
+    "url": "/static/media/multisequences.5630ba23.jpeg"
+  },
+  {
+    "revision": "c8d7e65206a0af4d0d4e1a043861e3e1",
+    "url": "/static/media/multisequences.c8d7e652.png"
+  },
+  {
+    "revision": "7d3e39bff39d94fbcc88c343449981c2",
+    "url": "/static/media/pointcontiguous.7d3e39bf.png"
+  },
+  {
+    "revision": "8a77ed5621a61f87503b9bf94cf079e9",
+    "url": "/static/media/pointsparse.8a77ed56.png"
+  },
+  {
+    "revision": "d9c53c81e40d4e93684707cdae82466a",
+    "url": "/static/media/segmentcontiguous.d9c53c81.png"
+  },
+  {
+    "revision": "739d9feb4b257d7db046f3a5b4b4e41d",
+    "url": "/static/media/segmentsparse.739d9feb.png"
+  },
+  {
+    "revision": "348892adc9e1607c5b9f1709ed46e6ff",
+    "url": "/static/media/singleroi.348892ad.jpeg"
+  },
+  {
+    "revision": "4131dd1e487e5aa74fea3369b1ecd406",
+    "url": "/static/media/singleroi.4131dd1e.png"
+  },
+  {
+    "revision": "28914c079fd8b74869a5028548ed9f78",
+    "url": "/static/media/summarize.28914c07.png"
+  }
+]);
