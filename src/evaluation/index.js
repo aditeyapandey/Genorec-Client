6315dbabcbd628da40e8bcf4679c636d7df48d2e
Aditeya Pandey <aditeyapandey@gmail.com>
2021-08-21 13:45:24 -0400

    Push the evaluation page, index.js to remote repo.

---
+++ src/evaluation/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+
+class Evaluation extends React.Component{
+
+  constructor(props) {
+    super(props);
+  }
+
+
+  render(){
+    return(
+      <>
+           
+        <h3> Testing Evaluation</h3>
+  
+      </>
+    );
+  }
+}
+
+
+export default Evaluation;
