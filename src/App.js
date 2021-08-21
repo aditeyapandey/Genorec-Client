cc03291c454e4ef8cb266e4d5782c5f0c88b269d
Aditeya Pandey <aditeyapandey@gmail.com>
2021-08-21 10:54:37 -0400

    Routing in Genorec-Client.
    1. In app.js we are using BrowserRouter to route betweent the app and the evaluation pages.
    2. To route to app, the url needs to appended with /app, for e.g. localhost:3000/app
    3. To route to the evaluation, the url needs to appended with /evaluation, for e.g. localhost:3000/evaluation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React from 'react';
-import "./Inputpage/Inputpage"
-import './App.css';
-import Inputpage from './Inputpage/Inputpage';
+import React from "react";
+import "./App.css";
+import Inputpage from "./Inputpage/Inputpage";
+import Evaluation from "./evaluation/index";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
 
 class App extends React.Component{ 
 
@@ -26,36 +28,45 @@ class App extends React.Component{
   // }
   
 
- render(){
-  return (
-    <div className="App">
-      <Header className="Header"/>
-      <Inputpage />
-      <Footer />
-    </div>
-  );
-}
+  render(){
+    return (
+      <div className="App">
+        <Header className="Header"/>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/app">
+              <Inputpage />
+            </Route>
+            <Route path="/evaluation">
+              <Evaluation />
+            </Route>
+          </Switch>
+        </BrowserRouter>
+        <Footer />
+      </div>
+    );
+  }
 }
 
 function Header(){
   return(
     <>
-     <div className="w3-bar w3-white w3-border-bottom w3-xlarge">
-     <a href="#"  className="w3-bar-item w3-button w3-text-red w3-hover-red"><b><i className={"fa fa-th-list w3-margin-right"}></i>GenoRec</b></a>
-    </div>
+      <div className="w3-bar w3-white w3-border-bottom w3-xlarge">
+        <a href="#"  className="w3-bar-item w3-button w3-text-red w3-hover-red"><b><i className={"fa fa-th-list w3-margin-right"}></i>GenoRec</b></a>
+      </div>
     </>
-  )
+  );
 }
 
 function Footer(){
   return (
-  <>
-  <div style={{clear:"both"}} className="w3-container w3-center w3-border-top w3-opacity w3-margin-top">
-  <h5>MIT License</h5>
-  <p>Created by <a href="https://www.aditeyapandey.com/" target="_blank" rel="noopener noreferrer" className="w3-hover-text-green">Aditeya Pandey</a></p>
- </div>
-  </>
-  )
+    <>
+      <div style={{clear:"both"}} className="w3-container w3-center w3-border-top w3-opacity w3-margin-top">
+        <h5>MIT License</h5>
+        <p>Created by <a href="https://www.aditeyapandey.com/" target="_blank" rel="noopener noreferrer" className="w3-hover-text-green">Aditeya Pandey</a></p>
+      </div>
+    </>
+  );
 }
 
 
