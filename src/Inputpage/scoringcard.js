803cb5f2b085ab9f6870e82bc93465287d89f690
Aditeya Pandey <aditeyapandey@gmail.com>
2021-08-22 21:39:38 -0400

    Adding scorecard UI to RR.

---
+++ src/Inputpage/scoringcard.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+
+function ScoringCardUI(props){
+  console.log(props);
+  return (
+    <>
+      <div style={{display: props.display?"":"none"}} className="w3-container">
+        <h4> On a score 1-5, how willing are you to accept the recommendation.</h4>
+        <p> 
+          <label>Less Willing</label>  
+          <input className="w3-radio w3-margin" type="radio" name="score" value="1" />
+          <label>1</label>
+          <input className="w3-radio w3-margin" type="radio" name="score" value="2" />
+          <label>2</label>
+          <input className="w3-radio w3-margin" type="radio" name="score" value="3" />
+          <label>3</label>
+          <input className="w3-radio w3-margin" type="radio" name="score" value="4" />
+          <label>4</label>
+          <input className="w3-radio w3-margin" type="radio" name="score" value="5" />
+          <label>5</label>
+          <label className="w3-margin"> More Willing</label>  
+        </p>
+
+        <h5 style={{display: props.id === 1?"":"none"}}> After you have scored click on Submit to move to the next option.</h5>
+        <h5 style={{display: props.id === 2?"":"none"}}> After you have scored click on Next Task button on top to move to the next task.</h5>
+        <button style={{display: props.id === 1?"":"none"}} onClick={props.handler}> Submit </button>
+      </div>
+    </>
+  );
+}
+
+export default ScoringCardUI;
+
+
