f397e63dada0ee40fbd555ed2633019c2595a41c
Sehi L'Yi <sehilyi@gmail.com>
2021-09-03 10:57:39 -0400

    Update base HTML file

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Genorec-Client/precache-manifest.b9d61ce5e1a3da9ab6a1db1f6763c8a1.js"
+  "/Genorec-Client/precache-manifest.90a207c1a9fad9f3ef890dc01d95cc9b.js"
 );
 
 self.addEventListener('message', (event) => {
